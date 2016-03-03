8c88e0ba569809288810b5bc7ddf67889bde19a0
Fred Sladkey <fsladkey@gmail.com>
2016-03-03 07:51:55 -0500

    Ensure authors are logged in before allowing them to view posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,10 @@ class ApplicationController < ActionController::Base
     session[:session_token] = nil
   end
 
+  private
+
+  def ensure_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :ensure_logged_in
   # #======= JSON post =========
   # "post" {
   #   "title": "something"
